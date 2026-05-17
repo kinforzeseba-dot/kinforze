d7803fcddf1660bc4af08dd6b6bc07d0ecb0dc5e
kinforzeseba-dot <kinforze.seba@gmail.com>
2026-05-17 02:26:21 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+window.KINFORZE_CONFIG = {
+  professionalsCsvUrl: "https://docs.google.com/spreadsheets/d/1_c6JDsASgDTOa7J1ultHN7Aa5Zp9OI_PDEoKdMGXJVI/gviz/tq?tqx=out:csv&gid=0",
+  materialsCsvUrl: "",
+};
